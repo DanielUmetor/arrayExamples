cbcc034f66515b89db5cd3bb507d0058e74fc52c
DanielUmetor <dnumetor@gmail.com>
2024-05-28 12:05:48 +0200

    arrayExamples

--- script/code.js
+++ script/code.js
@@ -1,6 +1,9 @@
 // ===How to create an array===
 // Array literal
 let numbers = [9, 3, 8, 11]
+console.log(numebrs);
+// Add a new element
+numbers.push
 // Array constructor
 let data = new Array('Khanya', 'John', 15, 18)
 // Array.of ()
@@ -8,4 +11,4 @@ let myArr1 = Array.of(9, 2, 4)
 // Array.from
 let myArr2 = Array.from(numbers)
 // Array.fill
-let emoji = new Array(4).fill('')
+let emoji = new Array(4).fill('👍')  
